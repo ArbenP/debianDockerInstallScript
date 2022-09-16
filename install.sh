5e5cd268e88db377e02c267cfc33070f83ad9523
Arben P <62248907+ArbenP@users.noreply.github.com>
2022-09-16 10:13:13 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+`#/bin/bash
+echo "This will install Docker, docker compose and Portainer for Ubuntu/Debian based distros"
+apt update && apt upgrade -y
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce -y
+apt install docker-compose-plugin
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+echo "done installing! You can run docker ps to see portainer running"`
