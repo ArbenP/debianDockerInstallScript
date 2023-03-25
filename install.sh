da75b4ce91d671295a5c32bc5a30d5147bd53977
Arben P <62248907+ArbenP@users.noreply.github.com>
2023-03-25 21:25:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
-`#/bin/bash
+#!/bin/bash
+set -e
+ARCH="$(arch)"
+
+if [ "$ARCH" != "aarch64" ]; then
+	echo "This script is only for x86 not for ARM."
+	exit 1
+fi
+
 echo "This will install Docker, docker compose and Portainer for Ubuntu/Debian based distros"
-apt update && apt upgrade -y
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+apt-get update && apt-get upgrade -y
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce -y
-apt install docker-compose-plugin
+apt-get -y install docker-ce -y
+apt-get -y install docker-compose-plugin
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-echo "done installing! You can run docker ps to see portainer running"`
+echo ""
+echo "Portainer is running on port 9443 through HTTPS."
